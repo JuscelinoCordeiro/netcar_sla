d65eba3f43803dad8fece97945dc3b6d4ebddb0a
Juscelino Cordeiro <jfilhodev@gmail.com>
2019-03-19 23:39:52 -0300

    commit

--- application/controllers/C_agendamento.php
+++ application/controllers/C_agendamento.php
@@ -1,29 +1,31 @@
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php
 
-class C_agendamento extends My_Controller{
-    function __construct(){
-        parent::__construct();
-        $this->load->model('m_agendamento');
-    }
+    defined('BASEPATH') OR exit('No direct script access allowed');
 
-    public function index(){
-        
-    }
+    class C_agendamento extends My_Controller {
 
-    public function listarAgendamentosDoDia(){
-        $dados['agendamentos_dia'] = $this->m_agendamento->getAgendamentosDoDia();
-        $dados['titulo'] = "Agendamentos do dia";
-        $this->showTemplate("v_agendamento_listar", $dados);
-    }
+        function __construct() {
+            parent::__construct();
+            $this->load->model('m_agendamento');
+        }
 
-    public function listarAgendamentos(){
-        $dados['agendamentos'] = $this->m_agendamento->getAgendamentos();
-        $dados['titulo'] = "Agendamentos";
-        $this->showTemplate("v_agendamento_todos", $dados); 
-    }
+        public function index() {
 
-    public function addAgendamento(){
+        }
+
+        public function listarAgendamentosDoDia() {
+            $dados['agendamentos_dia'] = $this->m_agendamento->getAgendamentosDoDia();
+            $dados['titulo'] = "Agendamentos do dia";
+            $this->showTemplate("v_agendamento_listar", $dados);
+        }
+
+        public function listarAgendamentos() {
+            $dados['agendamentos'] = $this->m_agendamento->getAgendamentos();
+            $dados['titulo'] = "Agendamentos";
+            $this->showTemplate("v_agendamento_todos", $dados);
+        }
+
+        public function addAgendamento() {
             if (($this->input->post('acao') !== null) && ($this->input->post('acao') === "novoAgendamento" )) {
                 $dados['cd_usuario'] = $this->input->post('cd_usuario');
                 $dados['cd_tpveiculo'] = $this->input->post('tipo_veiculo');
@@ -33,19 +35,17 @@ class C_agendamento extends My_Controller{
                 $dados['horario'] = $this->input->post('horario');
                 $dados['status'] = $this->input->post('status');
                 $valida['mensagem'] = $this->m_usuario->addAgendamento($dados);
-                 
+
                 unset($dados);
-    
+
                 $this->showAjax('v_mensagem', $valida);
             } else {
                 $dados['titulo'] = "Agendamento";
 
                 $this->showTemplate('v_agendamento_adicionar', $dados);
             }
-    
+        }
+
     }
-
-}
-
 
 ?>
